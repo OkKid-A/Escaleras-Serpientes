4eac5733422e32bdce7279d12da087349d9e921b
Miguel Quemé <miguelqueme202031398@cunoc.edu.gt>
2021-06-29 22:35:25 -0600

    Ahora se pueden agregar Jugadores

--- src/main/java/InterfazGrafica/VentanaEligirJugadores.java
+++ src/main/java/InterfazGrafica/VentanaEligirJugadores.java
@@ -1,4 +1,12 @@
 package InterfazGrafica;
 
-public class VentanaEligirJugadores {
+import javax.swing.*;
+
+public class VentanaEligirJugadores extends JFrame implements Ventana {
+
+    
+    @Override
+    public void fixComponents(JFrame frame, JPanel panel) {
+
+    }
 }
